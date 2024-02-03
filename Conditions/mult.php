21d121847f6fbc13bfb13d83a7deda4844fb6fc0
Shalahuddin <mdshalahuddin004@gmail.com>
2024-02-03 21:30:50 +0600

    loop for multiple 10

---
+++ Conditions/mult.php
@@ -0,0 +1,11 @@
+<?Php
+// 1 - 10 multiple
+
+for($i = 1; $i <= 10; $i++){
+    echo ' <h1> Multiple : '.$i.'</h1> ';
+for($j=1; $j <= 10; $j++){ echo $i.'X'.$j.'='.$i*$j.' <br>';
+    }
+
+    }
+
+    ?>
