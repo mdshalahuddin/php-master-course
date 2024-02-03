3789c243143e99064e81b34bef59a148c75a33bc
Shalahuddin <mdshalahuddin004@gmail.com>
2024-02-04 01:02:38 +0600

    all loop

---
+++ loop/index.php
@@ -0,0 +1,44 @@
+<?php
+
+$names = ['sadek','ariful','sabir','kamrul'];
+$users =[
+    ['name'=>'rasel','email'=>'rasel@Email.com'],
+    ['name'=>'nasir','email'=>'nasir@Email.com'],
+    ['name'=>'ariful','email'=>'ariful@Email.com'],
+    ['name'=>'samad','email'=>'samad@Email.com']
+]
+
+?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">Php</h1>
+        </div>
+    </header>
+    <div class="container p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+            <h3 class="text-xl font-semibold mb-4">Using a for loop</h3>
+            <ul class="mb-6"></ul>
+            <h3 class="text-xl font-semibold mb-4">Using a foreach loop</h3>
+            <ul class="mb-6"></ul>
+            <h3 class="text-xl font-semibold mb-4">Using a foreach loop with index</h3>
+            <ul class="mb-6"></ul>
+            <h3 class="text-xl font-semibold mb-4">Using a foreach loop with associative array</h3>
+            <ul class="mb-6"></ul>
+            <h3 class="text-xl font-semibold mb-4">getting key names and value from associative array </h3>
+            <ul class="mb-6"></ul>
+
+        </div>
+    </div>
+</body>
+
+</html>
